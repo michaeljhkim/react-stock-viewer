733d9e66cf0fb19ba47e48ff89d19a176ea9fb76
mk <michaelkim291@gmail.com>
2024-08-13 19:17:17 -0400

    Requests using axios probably works now

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-//import './r.js'
-//import XMLHttpRequest from 'xhr2'
-import 'requirejs'
+import axios from "axios"
 import './index.css'
 
 
-console.log( "test" );
+//console.log( "test 1" );
 
 'use strict';
-var request = require('request');
 
 // replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
 var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=IT2LGUXQ1XBM9NWY';
-
-request.get({
+/*
+axios.get({
     url: url,
     json: true,
     headers: {'User-Agent': 'request'}
@@ -28,4 +25,24 @@ request.get({
       // data is successfully parsed as a JSON object:
       console.log(data);
     }
+    console.log( "test 2" );
 });
+*/
+
+
+axios.get({
+  url: url,
+  json: true,
+  headers: {'User-Agent': 'request'}
+  })
+  .then(function (res) {
+    if (res.statusCode !== 200) {
+      console.log('Status:', res.statusCode);
+    }
+  })
+  .catch(function (err) {
+    console.log('Error:', err);
+  })
+  .finally(function (data) {
+    console.log(data);
+  });
